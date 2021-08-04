7dd2a87574003731d2562240d35b94f5883eebfb
MattMakesNoise <72995030+MattMakesNoise@users.noreply.github.com>
2021-08-04 16:05:00 +0100

    adds gulp set up
    -compiles SCSS.
    -complies JS to ES5 using Babel.
    -minifies CSS and JS for production.
    -enables live reload for all browsers inc mobiles.

---
+++ dev-saved/main.es5.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Let me go home
+var name = "Matt";
+
+var add = function add(a, b) {
+  return a + b;
+};
+
+var num = 4; //This shouldn't be seen once minified!
---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Let me go home
+let name = `Matt`;
+let add = (a,b) => a + b;
+
+const num = 4;
+
+//This shouldn't be seen once minified!
