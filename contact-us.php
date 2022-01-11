21b650723dda941d5b39f6660857c48c7cb005dd
MattMakesNoise <72995030+MattMakesNoise@users.noreply.github.com>
2022-01-11 09:28:41 +0000

    includes the header and footer and starts adding the HTML for the contact page

---
+++ contact-us.php
@@ -0,0 +1,74 @@
+<?php
+    include('inc/header.php'); 
+?>
+<!-- TOP BANNER START -->
+<div class="banner-wrapper">
+    <h1>Our offices</h1>
+</div>
+<!-- TOP BANNER END -->
+
+<!-- LOCATION CARDS START -->
+<div class="location-card-container">
+    <div class="location-card">
+        <div class="news-card scion-card">
+            <div class="news-card-main">
+                <div class="img-wrap">
+                    <div class="card-img-top scion">
+                        <a href="#">News</a>
+                        <img src="img/congratulations-to-lloyd-OGgS.jpg" alt="New Scion Graduate">
+                    </div>
+                </div>
+                <div class="news-body">
+                    <a class="scion-title">Congratulations to Lloyd Cox for Graduating t...</a>
+                    <p class="news-text">We are pleased to announce that after a year spent on our Netmatters Scion Coalition Scheme (...</p>
+                    <a href="#" class="scion-btn news-btn">Read More</a>
+                </div>
+            </div>
+            <div class="card-footer">
+                <img src="img/netmatters-ltd-VXAv.png" class="rounded-circle float-start" alt="Netmatters">
+                <div class="user-date">
+                    <p class="posted-by">Posted by Netmatters Ltd</p>
+                    <small class="text-muted">10th May 2021</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="location-map">
+        
+    </div>
+    <div class="location-card">
+
+    </div>
+    <div class="location-map">
+
+    </div>
+    <div class="location-card">
+
+    </div>
+    <div class="location-map">
+
+    </div>
+</div>
+<!-- LOCATION CARDS END -->
+
+<!-- OPENING TIMES START -->
+<div class="opening-times-wrapper">
+    <h4 class="opening-times-header">
+
+    </h4>
+</div>
+<!-- OPENING TIMES END -->
+
+<!-- ENQUIRY FORM START -->
+<div class="enquiry-form-wrapper">
+    <form action="" class="enquiry-form">
+
+    </form>
+</div>
+<!-- ENQUIRY FORM END -->
+
+
+
+<?php
+    include('inc/footer.php'); 
+?>
---
+++ index.php
@@ -0,0 +1,308 @@
+<?php
+    include('inc/header.php'); 
+?>
+
+
+
+    
+
+        <!-- HERO -->
+        <div class="hero-wrapper hero-mt">    
+            <div class="carousel">
+                <div class="slide web-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">Web Design</h2>
+                            <h4 class="hero-subtitle">For businesses looking to make a strong <br> and effective first impression.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button web-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide it-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">IT Support</h2>
+                            <h4 class="hero-subtitle">Fast and cost-effective IT support<br> services for your business.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button it-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide tele-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">Telecoms Services</h2>
+                            <h4 class="hero-subtitle">A new approach to connectivity, see how <br> we can help your business.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button tele-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide soft-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">Bespoke Software</h2>
+                            <h4 class="hero-subtitle">Bring you business together <br> with solutions that grow with you.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button soft-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide market-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">Digital Marketing</h2>
+                            <h4 class="hero-subtitle">Generating your new business through <br> results-driven marketing activities.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button market-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slide cyber-slide">
+                    <div class="hero-image-overlay">
+                    </div>
+                    <div class="hero-text-outer">
+                        <div class="hero-text-wrapper">
+                            <h2 class="hero-title">Cyber Security</h2>
+                            <h4 class="hero-subtitle">Keeping businesses and their customers <br> sensitive information protected.</h4>
+                            <div class="hero-button-wrapper">
+                                <button class="hero-button cyber-button">Find Out More <span class="hero-icon"><i class="fas fa-arrow-right"></i></span></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero End -->
+        
+        <!-- Cards -->
+        <div class="cards-wrapper">
+            <div class="cards-section">
+                <div class="row-one"> <!-- TOP THREE CARDS -->
+                    <a href="" class="card-link">
+                        <div class="column card soft-card"> 
+                            <div class="icon-border">
+                                <span class="fas fa-th card-icon"></span>
+                            </div>
+                            <h5 class="card-title">Bespoke Software</h5>
+                            <p class="card-text">Tailored software solutions to improve business productivity and online profits. Our expert team will ensure a software solution.</p>
+                            <div class="soft-btn card-btn">Read More</div>
+                        </div>
+                    </a>
+                    <a href="" class="card-link">
+                        <div class="column card it-card">
+                            <div class="icon-border">
+                                <i class="fas fa-desktop card-icon"></i>
+                            </div>
+                            <h5 class="card-title">IT Support</h5>
+                            <p class="card-text">Remotely managed IT services that is catered to your business needs, adds value and reduces costs.</p>
+                            <div class="it-btn card-btn">Read More</div>
+                        </div>
+                    </a>
+                    <a href="" class="card-link">
+                        <div class="column card market-card">
+                            <div class="icon-border">
+                                <i class="fas fa-signal card-icon"></i>
+                            </div>
+                            <h5 class="card-title">Digital Marketing</h5>
+                            <p class="card-text">Driving brand awareness and ROI through creative digital marketing campaigns. We review and monitor online performances.</p>
+                            <div class="market-btn card-btn">Read More</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="row-double"> <!-- BOTTOM FOUR CARDS CONTAINER -->
+                    <div class="row-two"> <!-- 1ST PAIR BOTTOM CARDS -->
+                        <a href="" class="card-link">
+                            <div class="column card tele-card">
+                                <div class="icon-border">
+                                    <i class="fas fa-phone-volume card-icon"></i>
+                                </div>
+                                <h5 class="card-title">Telecoms Services</h5>
+                                <p class="card-text">Stay connected with bespoke telecoms solutions when you needs it most.</p>
+                                <div class="tele-btn card-btn">Read More</div>
+                            </div>
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="column card web-card">
+                                <div class="icon-border">
+                                    <i class="fas fa-code card-icon"></i>
+                                </div>
+                                <h5 class="card-title">Web Design</h5>
+                                <p class="card-text">User-centric design for businesses looking to make a lasting first impression.</p>
+                                <div class="web-btn card-btn">Read More</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="row-three"> <!-- 2ND PAIR BOTTOM CARDS -->
+                        <a href="" class="card-link">
+                            <div class="column card cyber-card">
+                                <div class="icon-border">
+                                    <i class="fas fa-shield-virus card-icon"></i>
+                                </div>
+                                <h5 class="card-title">Cyber Security</h5>
+                                <p class="card-text">Ensuring your online busines stays secure 24/7, 365 days of the year.</p>
+                                <div class="cyber-btn card-btn">Read More</div>
+                            </div>
+                        </a>
+                        <a href="" class="card-link">
+                            <div class="column card train-card">
+                                <div class="icon-border">
+                                    <i class="fas fa-graduation-cap card-icon"></i>
+                                </div>
+                                    <h5 class="card-title">Developer Training</h5>
+                                    <p class="card-text">Have you considered a career in web development but you aren't sure where to start?</p>
+                                    <div class="train-btn card-btn">Read More</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Cards End -->
+
+        <!-- About -->
+        <div class="about-wrapper">
+            <section class="about">
+                <h1 class="about-title">NETMATTERS</h1>
+                <h3 class="about-intro">Netmatters Ltd is a leading web design, IT support and digital marketing agency based in the East of England, with offices in Cambridge, Wymondham and Great Yarmouth.</h3>
+                <p class="about-p1">Founded in 2008, we work with businesses from a variety of industries to gain new prospects, nurture existing leads and further grow their sales.</p>
+                <p class="about-p2">We provide cost effective, reliable solutions to a range of services; from bespoke cloud-based management systems, workflow and IT solutions through to creative website development and integrated digital campaigning.</p>
+                <button type="button" class="culture-btn">Our Culture <span class="about-icon"><i class="fas fa-arrow-right"></i></span></button>
+            </section>
+        </div>
+        <!-- About End -->
+
+        <!-- News -->
+        <div class="news-wrapper">
+            <div class="news-title">
+                <h2 class="news-title-text">LATEST</h2>
+            </div>
+            <div class="news">
+                <div class="news-cards-wrapper">
+                    <div class="news-card scion-card">
+                        <div class="news-card-main">
+                            <div class="img-wrap">
+                                <div class="card-img-top scion">
+                                    <a href="#">News</a>
+                                    <img src="img/congratulations-to-lloyd-OGgS.jpg" alt="New Scion Graduate">
+                                </div>
+                            </div>
+                            <div class="news-body">
+                                <a class="scion-title">Congratulations to Lloyd Cox for Graduating t...</a>
+                                <p class="news-text">We are pleased to announce that after a year spent on our Netmatters Scion Coalition Scheme (...</p>
+                                <a href="#" class="scion-btn news-btn">Read More</a>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <img src="img/netmatters-ltd-VXAv.png" class="rounded-circle float-start" alt="Netmatters">
+                            <div class="user-date">
+                                <p class="posted-by">Posted by Netmatters Ltd</p>
+                                <small class="text-muted">10th May 2021</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news-card notable-card">
+                        <div class="news-card-main">
+                            <div class="img-wrap">
+                                <div class="card-img-top notable">
+                                    <a href="#">News</a>
+                                    <img src="img/april-2021-notables-0C1u.jpg" alt="New Scion Graduate">
+                                </div>
+                            </div>
+                            <div class="news-body">
+                            <a class="notable-title">April 2021 Notables</a>
+                            <p class="news-text">Every month we celebrate the most notable of the employees here at Netmatters. Each department he...</p>
+                            <a href="#" class="notable-btn news-btn">Read More</a>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <img src="img/netmatters-ltd-VXAv.png" class="rounded-circle float-start" alt="Netmatters">
+                            <div class="user-date">
+                                <p class="posted-by">Posted by Netmatters Ltd</p>
+                                <small class="text-muted">6th May 2021</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news-card job-card">
+                        <div class="news-card-main">
+                            <div class="img-wrap">
+                                <div class="card-img-top job">
+                                    <a href="#">Careers</a>
+                                    <img src="img/web-developer-r6Y3.jpg" class="card-img-top" alt="New Scion Graduate">
+                                </div>
+                            </div>
+                            <div class="news-body">
+                            <a class="job-title">Web Developer</a>
+                            <p class="news-text">Salary Range £25,000 - £45,000 (DOE) + Bonus Hours 40 hours per week, Monday - Flexible Opt...</p>
+                            <a href="#" class="job-btn news-btn">Read More</a>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <img src="img/simon-wright-7UFt.jpg" class="rounded-circle float-start" alt="Simon Wright">
+                            <div class="user-date">
+                                <p class="posted-by">Posted by Simon Wright</p>
+                                <small class="text-muted">1st May 2021</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- News End -->
+
+        <!-- Clients -->
+        <div class="clients-wrapper">
+            <section class="clients">
+                <div class="busseys-container show">
+                    <div class="busseys-tooltip tooltip">
+                        <h3 class="client-title">Busseys</h3>
+                        <p>One of the UK's leading Ford dealerships.</p>
+                    </div>
+                </div>
+                <a href="#">
+                <div class="crane-container show">
+                    <div class="crane-tooltip tooltip">
+                        <h3 class="client-title">Crane Garden Buildings</h3>
+                        <p>Leading manufacturer and supplier of high-end garden rooms, summerhouses, workshops and sheds in the UK.</p>
+                    </div>
+                </div>
+                </a>
+                <div class="beat-container show">
+                    <div class="beat-tooltip tooltip">
+                        <h3 class="client-title">Beat</h3>
+                        <p>The Uk's eating disorder charity founded in 1989.</p>
+                    </div>
+                </div>
+                <a href="#">
+                <div class="diver-container show">
+                    <div class="diver-tooltip tooltip">
+                        <h3 class="client-title">Northern Diver</h3>
+                        <p>Global water based equipment manufacturers for sport, military, commercial and rescue businesses.</p>
+                    </div>
+                </div>
+                </a>
+            </section>
+        </div>
+        <!-- Clients End -->
+
+        
+
+<?php
+    include('inc/footer.php'); 
+?>
