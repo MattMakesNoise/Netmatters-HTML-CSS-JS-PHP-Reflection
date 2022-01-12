a1bf57799d71d31bcdfe944fef0b4cdfa71f5c6a
MattMakesNoise <72995030+MattMakesNoise@users.noreply.github.com>
2022-01-12 14:28:55 +0000

    adds majority of the responsive styling

--- contact-us.php
+++ contact-us.php
@@ -30,8 +30,12 @@
                         Cambridge, <br>
                         CB4 0WS
                     </p>
-                    <a class="location-tel-no">01223 37 57 72</a>
-                    <a href="#" class="location-btn">View More</a>
+                    <div class="location-tel-wrapper">
+                        <a class="location-tel-no">01223 37 57 72</a>
+                    </div>
+                    <div class="location-btn-wrapper">
+                        <a href="#" class="location-btn">View More</a>
+                    </div>
                 </div>
             </div>
             <div id='map-cambridge'>
@@ -55,8 +59,12 @@
                         Wymondham, Norfolk <br>
                         NR18 0WZ
                     </p>
+                    <div class="location-tel-wrapper">
                     <a class="location-tel-no">01603 70 40 20</a>
-                    <a href="#" class="location-btn">View More</a>
+                    </div>
+                    <div class="location-btn-wrapper">
+                        <a href="#" class="location-btn">View More</a>
+                    </div>
                 </div>
             </div>
             <div id="map-wymondham">
@@ -80,8 +88,12 @@
                         Great Yarmouth, Norfolk <br>
                         NR31 7RA
                     </p>
-                    <a class="location-tel-no">01493 60 32 04</a>
-                    <a href="#" class="location-btn">View More</a>
+                    <div class="location-tel-wrapper">
+                        <a class="location-tel-no">01493 60 32 04</a>
+                    </div>
+                    <div class="location-btn-wrapper">
+                        <a href="#" class="location-btn">View More</a>
+                    </div>
                 </div>
             </div>
             <div id="map-yarmouth">
@@ -92,80 +104,85 @@
         
     </div>
     <!-- LOCATION CARDS END -->
-
-    <!-- OPENING TIMES START -->
-    <div class="opening-times-wrapper">
-        <p class="opening-times-p">
-            <strong>Email us on:</strong>
-            <br>
-        </p>
-        <p class="opening-times-p">
-            <a href="mailto:sales@netmatters.com" class="sales-email">sales@netmatters.com</a>
-        </p>
-        <p class="opening-times-p">
-            <strong>Business hours:</strong>
-        </p>
-        <p class="opening-times-p">
-            <strong>Monday - Friday 07:00 - 18:00&nbsp;</strong>
-        </p>
-        <div class="out-of-hours-wrapper">
-            <div class="out-of-hours-header" id="accordian-btn">
-                <h4>
-                    <!-- <a href=""> -->
-                        <p>Out of Hours IT Support</p>
-                        <span class="fas fa-chevron-down"></span>
-                    <!-- </a> -->
-                </h4>
-            </div>
-            <div class="out-of-hours-accordian">
-                <p>Netmatters IT are offering an Out of Hours service for Emergency and Critical Tasks</p>
-                <p>
-                    <strong>Monday - Friday 18:00 - 22:00</strong>
-                    <strong>Saturday 08:00 - 16:00</strong>
-                    <br>
-                    <strong>Sunday 10:00 - 18:00</strong>
-                </p>
-                <p>To log a critical task, you will need to call our main line number and select Option 2 to leave an Out of Hours&nbsp;voicemail. A technician will contact you on the number provided within 45 minutes of your call.&nbsp;</p>
-            </div>
-        </div>
-    </div>
-    <!-- OPENING TIMES END -->
-
-    <!-- ENQUIRY FORM START -->
-    <div class="enquiry-form-wrapper">
-        <form action="" class="enquiry-form">
-        <div class="boxes">
-            <div class="name-wrapper">
-                <label for="inputName" class="form-label">Your Name <span class="make-red">*</span></label>
-                <input type="text" class="form-control" id="inputName">
-            </div>
-            <div class="mail-wrapper">
-                <label for="inputEmail" class="form-label">Your Email <span class="make-red">*</span></label>
-                <input type="email" class="form-control" id="inputEmail">
-            </div>
-            <div class="tel-no-wrapper">
-                <label for="telephone" class="form-label">Your Telephone Number <span class="make-red">*</span></label>
-                <input type="tel" class="form-control" id="inputTel">
-            </div>
-            <div class="subject-wrapper">
-                <label for="subject" class="form-label">Subject <span class="make-red">*</span></label>
-                <input type="text" class="form-control" id="inputSubject">
-            </div>
-            <div class="message-wrapper">
-                <label for="message" class="form-label">Message <span class="make-red">*</span></label>
-                <input type="text" class="form-control" id="inputMessage">
+    <div class="bottom-wrapper">
+        <!-- OPENING TIMES START -->
+        <div class="opening-times-wrapper">
+            <p class="opening-times-p">
+                <strong>Email us on:</strong>
+                <br>
+            </p>
+            <p class="opening-times-p">
+                <a href="mailto:sales@netmatters.com" class="sales-email">sales@netmatters.com</a>
+            </p>
+            <p class="opening-times-p">
+                <strong>Business hours:</strong>
+            </p>
+            <p class="opening-times-p">
+                <strong>Monday - Friday 07:00 - 18:00&nbsp;</strong>
+            </p>
+            <div class="out-of-hours-wrapper">
+                <div class="out-of-hours-header" id="accordian-btn">
+                    <h4>
+                        <!-- <a href=""> -->
+                            <p>Out of Hours IT Support</p>
+                            <span class="fas fa-chevron-down"></span>
+                        <!-- </a> -->
+                    </h4>
+                </div>
+                <div class="out-of-hours-accordian">
+                    <p>Netmatters IT are offering an Out of Hours service for Emergency and Critical Tasks</p>
+                    <p>
+                        <strong>Monday - Friday 18:00 - 22:00</strong>
+                        <strong>Saturday 08:00 - 16:00</strong>
+                        <br>
+                        <strong>Sunday 10:00 - 18:00</strong>
+                    </p>
+                    <p>To log a critical task, you will need to call our main line number and select Option 2 to leave an Out of Hours&nbsp;voicemail. A technician will contact you on the number provided within 45 minutes of your call.&nbsp;</p>
+                </div>
             </div>
         </div>
-        <label class="checkbox-container">Please tick this box if you wish to receive marketing information from us.
-        Please see our <a href="#" target="_blank">Privacy Policy</a> for more information on how we use your data.
-            <input type="checkbox">
-            <span class="checkmark"></span>
-        </label>
-        <button type="submit" class="enquiry-btn">Send Enquiry</button>
-        </form>
-        
+        <!-- OPENING TIMES END -->
+
+        <!-- ENQUIRY FORM START -->
+        <div class="enquiry-form-wrapper">
+            <form action="" class="enquiry-form">
+            <div class="enquiry-boxes">
+                <div class="name-mail">
+                    <div class="name-wrapper">
+                        <label for="inputName" class="form-label">Your Name <span class="make-red">*</span></label>
+                        <input type="text" class="form-control" id="inputName">
+                    </div>
+                    <div class="mail-wrapper">
+                        <label for="inputEmail" class="form-label">Your Email <span class="make-red">*</span></label>
+                        <input type="email" class="form-control" id="inputEmail">
+                    </div>
+                </div>
+                <div class="tel-subject">
+                    <div class="tel-no-wrapper">
+                        <label for="telephone" class="form-label">Your Telephone Number <span class="make-red">*</span></label>
+                        <input type="tel" class="form-control" id="inputTel">
+                    </div>
+                    <div class="subject-wrapper">
+                        <label for="subject" class="form-label">Subject <span class="make-red">*</span></label>
+                        <input type="text" class="form-control" id="inputSubject">
+                    </div>
+                </div>
+                <div class="message-wrapper">
+                    <label for="message" class="form-label">Message <span class="make-red">*</span></label>
+                    <input type="text" class="form-control" id="inputMessage">
+                </div>
+            </div>
+            <label class="checkbox-container">Please tick this box if you wish to receive marketing information from us.
+            Please see our <a href="#" target="_blank">Privacy Policy</a> for more information on how we use your data.
+                <input type="checkbox">
+                <span class="checkmark"></span>
+            </label>
+            <button type="submit" class="enquiry-btn">Send Enquiry</button>
+            </form>
+            
+        </div>
+        <!-- ENQUIRY FORM END -->
     </div>
-    <!-- ENQUIRY FORM END -->
 </div>
 </main>
 
